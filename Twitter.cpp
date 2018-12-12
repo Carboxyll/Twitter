9312701b0cb78ad62587bd431a7d94e5c197fc07
JChristensen <jack.christensen@outlook.com>
2018-12-12 16:32:21 -0500

    Change indentation from tabs to spaces.

--- Twitter.cpp
+++ Twitter.cpp
@@ -1,10 +1,10 @@
 /*
   Twitter.cpp - Arduino library to Post messages to Twitter using OAuth.
   Copyright (c) NeoCat 2010-2011. All right reserved.
-  
+
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 // ver1.2.0 - Use <string.h>
@@ -21,55 +21,55 @@ Twitter::Twitter(const char *token) : token(token)
 
 bool Twitter::post(const char *msg)
 {
-	parseStatus = 0;
-	statusCode = 0;
-	if (client.connect(LIB_DOMAIN, 80)) {
-		client.println("POST http://" LIB_DOMAIN "/update HTTP/1.0");
-		client.print("Content-Length: ");
-		client.println(strlen(msg) + strlen(token) + 14);   // 14=strlen("token=&status=")
+    parseStatus = 0;
+    statusCode = 0;
+    if (client.connect(LIB_DOMAIN, 80)) {
+        client.println("POST http://" LIB_DOMAIN "/update HTTP/1.0");
+        client.print("Content-Length: ");
+        client.println(strlen(msg) + strlen(token) + 14);   // 14=strlen("token=&status=")
         client.println("Host: " LIB_DOMAIN);
-		client.println();
-		client.print("token=");
-		client.print(token);
-		client.print("&status=");
-		client.println(msg);
-	} else {
-		return false;
-	}
-	return true;
+        client.println();
+        client.print("token=");
+        client.print(token);
+        client.print("&status=");
+        client.println(msg);
+    } else {
+        return false;
+    }
+    return true;
 }
 
 bool Twitter::checkStatus(Print *debug)
 {
-	if (!client.connected()) {
-		if (debug)
-			while(client.available())
-				debug->print((char)client.read());
-		client.flush();
-		client.stop();
-		return false;
-	}
-	if (!client.available())
-		return true;
-	char c = client.read();
-	if (debug)
-		debug->print(c);
-	switch(parseStatus) {
-	case 0:
-		if (c == ' ') parseStatus++; break;  // skip "HTTP/1.1 "
-	case 1:
-		if (c >= '0' && c <= '9') {
-			statusCode *= 10;
-			statusCode += c - '0';
-		} else {
-			parseStatus++;
-		}
-	}
-	return true;
+    if (!client.connected()) {
+        if (debug)
+            while(client.available())
+                debug->print((char)client.read());
+        client.flush();
+        client.stop();
+        return false;
+    }
+    if (!client.available())
+        return true;
+    char c = client.read();
+    if (debug)
+        debug->print(c);
+    switch(parseStatus) {
+    case 0:
+        if (c == ' ') parseStatus++; break;  // skip "HTTP/1.1 "
+    case 1:
+        if (c >= '0' && c <= '9') {
+            statusCode *= 10;
+            statusCode += c - '0';
+        } else {
+            parseStatus++;
+        }
+    }
+    return true;
 }
 
 int Twitter::wait(Print *debug)
 {
-	while (checkStatus(debug));
-	return statusCode;
+    while (checkStatus(debug));
+    return statusCode;
 }
--- Twitter.h
+++ Twitter.h
@@ -1,10 +1,10 @@
 /*
   Twitter.cpp - Arduino library to Post messages to Twitter using OAuth.
   Copyright (c) NeoCat 2010-2011. All right reserved.
-  
+
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 // ver1.2.0 - Use <Udp.h> to support IDE 0019 or later
@@ -22,17 +22,17 @@
 class Twitter
 {
 private:
-	uint8_t parseStatus;
-	int statusCode;
-	const char *token;
-	EthernetClient client;
+    uint8_t parseStatus;
+    int statusCode;
+    const char *token;
+    EthernetClient client;
 public:
-	Twitter(const char *user_and_passwd);
-	
-	bool post(const char *msg);
-	bool checkStatus(Print *debug = NULL);
-	int  wait(Print *debug = NULL);
-	int  status(void) { return statusCode; }
+    Twitter(const char *user_and_passwd);
+
+    bool post(const char *msg);
+    bool checkStatus(Print *debug = NULL);
+    int  wait(Print *debug = NULL);
+    int  status(void) { return statusCode; }
 };
 
 #endif
